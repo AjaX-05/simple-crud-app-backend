156373f029225860278846ac52c99d6b704e9f00
AjaX-05 <majaysakthishankar@gmail.com>
2024-09-25 20:12:19 -0500

    Created a Simple Backend CRUD APP

---
+++ CRUD API/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const Product = require("./models/product.model");
+const productRoute = require("./routes/product.route");
+
+const mongoose = require("mongoose");
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+app.use("/api/products", productRoute);
+
+app.get("/", (req, res) => {
+  console.log(req.body);
+  res.status(200).send("Helo Wrld!");
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://admin2:AjayShankar@cluster0.7cghn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("Connected to DB");
+    app.listen(5000, () => {
+      console.log("Listening on port 5000");
+    });
+  })
+  .catch((err) => {
+    console.log("Error connecting:", err);
+  });
